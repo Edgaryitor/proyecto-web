5db8f44dddb0bad0538087c08196d74e696321b4
EstefaniaMartinez11 <158644147+EstefaniaMartinez11@users.noreply.github.com>
2024-11-28 20:24:22 -0600

    Create srcipt.js

---
+++ js/srcipt.js
@@ -0,0 +1,7 @@
+function mostrarVentana() {
+    document.getElementById('ventanaEmergente').style.display = 'flex';
+}
+
+function cerrarVentana() {
+    document.getElementById('ventanaEmergente').style.display = 'none';
+}
